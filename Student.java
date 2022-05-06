a68336b24637e05b2115beffb1a4d376fc4b6bb4
Liam Csiffary <liam.csiffary@stu.ocsb.ca>
2022-05-06 09:44:02 -0400

    overcomplicated the code, can now edit all properties of each user, will still work if two or more students share the same first name. Comments and header completed.

--- Student.java
+++ Student.java
@@ -1,3 +1,13 @@
+/**
+ * Student.java is class of student, it has
+ * properties: first name, middle initial, last name
+ * grade and IEP
+ *
+ * @author Liam Csiffary
+ * @version 1.1
+ * @since 2022-05-06
+ */
+
 public class Student {
   String firstName;
   String midInit;
@@ -5,7 +15,7 @@ public class Student {
   int grade;
   boolean IEP;
 
-
+  // constructor
   public Student(String firstName, String midInit, String lastName, int grade, boolean IEP) {
     this.firstName = firstName;
     this.midInit = midInit;
@@ -14,10 +24,31 @@ public class Student {
     this.IEP = IEP;
   }
 
+  // student printer
   public void print() {
     System.out.println("Name (First Middle. Last): " + firstName + " " + midInit + ". " + lastName);
     System.out.println("Grade: " + grade);
     System.out.println("IEP: " + IEP);
+  }
+
+  public void editFirst(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void editMiddle(String midInit) {
+    this.midInit = midInit;
+  }
+
+  public void editLast(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void editGrade(int grade) {
+    this.grade = grade;
+  }
+
+  public void editIEP(boolean IEP) {
+    this.IEP = IEP;
   }
   
 }
--- StudentGenerator.java
+++ StudentGenerator.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * StudentGenerator.java is a program which generates students
+ * students from a txt file and allows the user to edit their information.
+ *
+ * @author Liam Csiffary
+ * @version 1.1
+ * @since 2022-05-06
+ */
+
 public class StudentGenerator {
 
   // generates the array of inputs from a text file
@@ -14,7 +23,7 @@ public class StudentGenerator {
     System.out.println(file);
     Scanner scStudent;
     String[] tempArr = new String[0];
-    
+
     try {
       scStudent = new Scanner(file);
     } catch (FileNotFoundException e) {
@@ -41,13 +50,70 @@ public class StudentGenerator {
 
     return array;
   }
-  
+
+  // gets the student to edit from the user
+  public static int getStudent(List<Student> studentList) {
+    // in case thers more than 1 student with the same first name
+    List<Integer> numStudents = new ArrayList<Integer>();
+    Scanner sc = new Scanner(System.in);
+
+    // gets the student to edit
+    while (true) {
+      System.out.println("Which student would you like to edit?");
+
+      for (int i = 0; i < studentList.size(); i++) {
+        if (studentList.size() < i) {
+          System.out.println(studentList.get(i).firstName + ", or ");
+        } else {
+          System.out.println(studentList.get(i).firstName);
+        }
+      }
+
+      String userInput = sc.nextLine().toLowerCase();
+
+      // checks to see if the users input matches one of the students
+      for (int i = 0; i < studentList.size(); i++) {
+        if (userInput.equals((studentList.get(i).firstName).toLowerCase())) {
+          numStudents.add(i);
+        }
+      }
+
+      // if there is 1 student with that first name
+      if (numStudents.size() == 1) {
+        System.out.println("You are editing " + studentList.get(numStudents.get(0)).firstName);
+        return numStudents.get(0);
+
+        // if there is more than one student with that first name
+      } else if (numStudents.size() > 1) {
+        System.out.println("There is more than 1 student by that first name.");
+        System.out.println("Which student would you like to edit? Please input their last name.");
+
+        // prints the options to the user
+        for (int i = 0; i < numStudents.size(); i++) {
+          studentList.get(numStudents.get(i)).print();
+        }
+        // gets the students last name from the user
+        userInput = sc.nextLine().toLowerCase();
+
+        // if the users input matches one of the students last names then
+        // return the index of the student to edit
+        for (int i = 0; i < numStudents.size(); i++) {
+          if (userInput.equals((studentList.get(numStudents.get(i)).lastName).toLowerCase())) {
+            System.out.println("You are editing " + studentList.get(numStudents.get(i)).lastName);
+            return numStudents.get(i);
+          }
+        }
+      }
+    }
+  }
+
+  // main function
   public static void main(String[] args) {
+    // define vars
     Scanner scanner = new Scanner(System.in);
     List<Student> studentList = new ArrayList<Student>();
     Student student;
     String userInput;
-
 
     // gets users relative path
     String path = System.getProperty("user.dir");
@@ -57,68 +123,141 @@ public class StudentGenerator {
     String[] students = arrayGenerator(file);
     System.out.println("\n");
 
+    // generates the students from the array sent back from the txt file
     for (int i = 0; i < students.length; i += 5) {
-      if (students[i].equals("")) {
+      // in case there are blank lines
+      while (students[i].equals("")) {
         i++;
       }
+      // if there is no middle name, indicated in the txt file by an underscore
       if (students[i + 1].equals("_")) {
-        student = new Student(students[i], "N.M.N", students[
-          i + 2], Integer.parseInt(students[i + 3]), Boolean.parseBoolean(students[i + 4]));
+        student = new Student(students[i], "N.M.N", students[i + 2],
+            Integer.parseInt(students[i + 3]), Boolean.parseBoolean(students[i + 4]));
 
       } else {
-        student = new Student(students[i], students[i + 1], students[
-          i + 2], Integer.parseInt(students[i + 3]), Boolean.parseBoolean(students[i + 4]));
+        student = new Student(students[i],
+            String.valueOf(students[i + 1].charAt(0)).toUpperCase(),
+            students[i + 2], Integer.parseInt(students[i + 3]),
+            Boolean.parseBoolean(students[i + 4]));
 
       }
       studentList.add(student);
     }
 
+    // prints all the students
     for (int i = 0; i < studentList.size(); i++) {
       studentList.get(i).print();
     }
 
+    // students editor
     while (true) {
-      System.out.println("Would you like to edit these students? (y/n)");
+      System.out.println("Would you like to edit these students? (y/n) or view students? (view)");
       userInput = scanner.nextLine().toLowerCase();
       if (userInput.equals("y")) {
-        while (true) {
-          System.out.println("Which student would you like to edit?");
+        int whichStu;
 
-          for (int i = 0; i < studentList.size(); i++) {
-            if (studentList.size() < i) {
-              System.out.println(studentList.get(i).firstName + ", or ");
-            } else {
-              System.out.println(studentList.get(i).firstName);
-            }
-          }
+        // gets the student to edit, moved to a funtion as it got pretty complicated
+        whichStu = getStudent(studentList);
+
+        // gets the property to edit
+        while (true) {
+          System.out.println("\nWhich property would you like to edit?");
+          System.out.println("First, middle, last,");
+          System.out.println("Grade, or IEP.\n");
 
           userInput = scanner.nextLine().toLowerCase();
 
-          boolean isStudent = false;
-          for (int i = 0; i < studentList.size(); i++) {
-            if (userInput.equals((studentList.get(i).firstName).toLowerCase())) {
-              System.out.println("You are editing " + studentList.get(i).firstName);
-              isStudent = true;
-              break;
-            }
-          }
-          if (isStudent) {
+          // change the first name to the user input
+          if (userInput.equals("first")) {
+            System.out.println("Please input new first name, be sure to capitalize.");
+            userInput = scanner.nextLine();
+            studentList.get(whichStu).editFirst(userInput);
             break;
+
+            // change the middle name, or simply remove it
+          } else if (userInput.equals("middle")) {
+            System.out.println("Please input new middle name. Input none for no middle name");
+            userInput = scanner.nextLine();
+
+            if (userInput.equals("none") || userInput.equals("None")) {
+              studentList.get(whichStu).editMiddle("N.M.N");
+
+            } else {
+              // gets the first letter of the middle name and
+              // capitalizes it if not already capitalized
+              studentList.get(whichStu).editMiddle(
+                  String.valueOf(userInput.charAt(0)).toUpperCase());
+
+            }
+            break;
+
+            // change last name to users input
+          } else if (userInput.equals("last")) {
+            System.out.println("Please input new last name, be sure to capitalize.");
+            userInput = scanner.nextLine();
+            studentList.get(whichStu).editLast(userInput);
+            break;
+
+            // chane grade of the student
+          } else if (userInput.equals("grade")) {
+            int userInputInt;
+            while (true) {
+              System.out.println("Please input new grade");
+              userInput = scanner.nextLine();
+
+              try {
+                userInputInt = Integer.parseInt(userInput);
+                if (userInputInt > 0) {
+                  break;
+                } else {
+                  System.out.println("Please input a natural number");
+                }
+              } catch (NumberFormatException e) {
+                continue;
+              }
+            }
+
+            studentList.get(whichStu).editGrade(userInputInt);
+            break;
+
+            // gets true of false for the IEP of the student
+          } else if (userInput.equals("IEP")) {
+            boolean IEPBool;
+            while (true) {
+              System.out.println("Please input true/false");
+              userInput = scanner.nextLine();
+
+              try {
+                IEPBool = Boolean.parseBoolean(userInput);
+                break;
+              } catch (Exception e) {
+                continue;
+              }
+            }
+
+            studentList.get(whichStu).editIEP(IEPBool);
+            break;
+
+          } else {
+            System.out.println("Not a valid input");
           }
         }
 
-        break;
+        // if the user does not want to make changes
       } else if (userInput.equals("n")) {
-
-
+        System.out.println("You are not editing any students");
         break;
+
+        // displays all students and their information
+      } else if (userInput.equals("view")) {
+        for (int i = 0; i < studentList.size(); i++) {
+          studentList.get(i).print();
+        }
+
       } else {
-        System.out.println("Please enter either y/n");
+        System.out.println("Please enter either 'y' or 'n'");
       }
     }
-
-
     scanner.close();
-
   }
 }
